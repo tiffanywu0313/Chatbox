5875d15002db991714e82981baa1a54efaeba223
Tiffany Wu <39014470+tiffanywu0313@users.noreply.github.com>
2018-09-22 02:16:29 -0700

    Add files via upload

---
+++ client/app.js
@@ -0,0 +1,13 @@
+//alert('hello from the JS file');
+var socket = io();
+
+$('form').submit(function(){
+  var text = $('#message').val();
+  socket.emit('message', text);
+  $('#message').val('');
+  return false;
+});
+
+socket.on('message', function (msg) {
+  $('<li>').text(msg).appendTo('#history');
+});
